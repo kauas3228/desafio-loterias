8c2089b55c58363d33b3ead99128bf5f736b9d76
kauas3228 <kauas3228@gmail.com>
2024-05-12 01:24:12 -0300

    Starting production left part

--- src/assets/Pages/Home/Home.tsx
+++ src/assets/Pages/Home/Home.tsx
@@ -7,24 +7,38 @@ import Dropdown from '../../components/DropDown/Dropdown';
 
 export default function Home() {
   const [lotteries, setLotteries] = useState([]);
-  const apiURL = 'https://loteriascaixa-api.herokuapp.com/api';
+  const apiURL = `https://loteriascaixa-api.herokuapp.com/api/megasena/latest`;
 
-  const getLotteries = async (url: any) =>{
+  const getLotteries = async (url: any) => {
     const res = await fetch(url);
     const data = await res.json();
 
     setLotteries(data);
   }
 
-    useEffect(() => {
-      const lotteries = apiURL;
+  useEffect(() => {
+    const lotteries = apiURL;
 
-      getLotteries(lotteries);
-    }, []);
+    getLotteries(lotteries);
+
+  }, []);
 
   return (
-    <div>
-        <Dropdown></Dropdown>
+    <div className='container'>
+      <div className='leftContainer'>
+        <Dropdown />
+
+        <div className='lotterieName'>
+          <img src='/Logo_Sena.svg'></img>
+          <h1>{lotteries.loteria}</h1>
+        </div>
+
+        <div className='lotterieInformations'>
+          <p>Concurso</p>
+          <p className='info'>{lotteries.concurso} - {lotteries.data}</p>
+        </div>
+
+      </div>
     </div>
   )
 }
