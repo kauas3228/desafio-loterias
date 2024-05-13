7b14cb6e2822b0a6d5e1330dd7ee7d227416b397
kauas3228 <kauas3228@gmail.com>
2024-05-13 11:36:04 -0300

    fix: fixing dropdown

--- src/assets/Pages/Home/Home.tsx
+++ src/assets/Pages/Home/Home.tsx
@@ -6,8 +6,14 @@ import './Home.css';
 import Dropdown from '../../components/DropDown/Dropdown';
 
 export default function Home() {
+  // Data
   const [lotteries, setLotteries] = useState([]);
-  const apiURL = `https://loteriascaixa-api.herokuapp.com/api/megasena/latest`;
+  const [seletValue, setSelectValue] = useState('megasena');
+
+  // API URL
+  const apiURL = `https://loteriascaixa-api.herokuapp.com/api/${seletValue}/latest`;
+
+  // Getting data for api
 
   const getLotteries = async (url: any) => {
     const res = await fetch(url);
@@ -23,10 +29,39 @@ export default function Home() {
 
   }, []);
 
+  // Dropdown
+
+  const itens = [
+    { id: 1, name: 'megasena' },
+    { id: 2, name: 'quina' },
+    { id: 3, name: 'lotofacil' },
+    { id: 4, name: 'lotomania' },
+    { id: 5, name: 'timemania' },
+    { id: 6, name: 'diadesorte' }
+  ];
+  const handleDropdown = () => {
+    const loterrie = apiURL;
+
+    getLotteries(loterrie);
+    console.log(lotteries);
+    console.log(seletValue);
+  }
+
   return (
     <div className='container'>
       <div className='leftContainer'>
-        <Dropdown />
+
+        <select value={seletValue} onChange={e => {
+          setSelectValue(e.target.value)
+          handleDropdown();
+        }
+        }>
+          {
+            itens.map((item) => (
+              <option value={item.name} key={item.id}>{item.name}</option>
+            ))
+          }
+        </select>
 
         <div className='lotterieName'>
           <img src='/Logo_Sena.svg'></img>
@@ -37,6 +72,10 @@ export default function Home() {
           <p>Concurso</p>
           <p className='info'>{lotteries.concurso} - {lotteries.data}</p>
         </div>
+
+      </div>
+
+      <div className='rigthContainer'>
 
       </div>
     </div>
